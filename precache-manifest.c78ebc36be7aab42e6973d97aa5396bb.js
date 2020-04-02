9ba01e26cd0d6fbf0bb7e7456c3a3540d9a91874
Thijs Dickmans <thijs@mbp.lan>
2020-04-02 19:56:25 +0200

    Initial dist subtree commit

---
+++ precache-manifest.c78ebc36be7aab42e6973d97aa5396bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27292b978638dd9297d",
+    "url": "/game-of-thrones-risk/css/app.53624e31.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/game-of-thrones-risk/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9f3b00c6cafe49ed508d7bdeca866994",
+    "url": "/game-of-thrones-risk/index.html"
+  },
+  {
+    "revision": "d5080be8a0c568af385a",
+    "url": "/game-of-thrones-risk/js/about.32b4ba4c.js"
+  },
+  {
+    "revision": "d27292b978638dd9297d",
+    "url": "/game-of-thrones-risk/js/app.e694146d.js"
+  },
+  {
+    "revision": "bc93f57b8fa0fc706345",
+    "url": "/game-of-thrones-risk/js/chunk-vendors.c20071ba.js"
+  },
+  {
+    "revision": "9d99c099629abc696b83592e262bb74e",
+    "url": "/game-of-thrones-risk/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/game-of-thrones-risk/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/game-of-thrones-risk/precache-manifest.c78ebc36be7aab42e6973d97aa5396bb.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "game-of-thrones-risk"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
